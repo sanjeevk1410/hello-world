3ed40441278dee192b402175399859c6877e9214
jrosinsk <joe.rosinski@oracle.com>
2017-12-04 17:13:44 -0800

    CROW e2e automation change

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	  w.Write([]byte("Hello friend!!\n"))
-       w.Write([]byte("AUTOMATION_TAG:26NesZXY8MeIHY0PdAYq2SQXowFhkYDcYcTDXxe.s6S49uOMjHz58zc9BZJDNcfC"))
+       w.Write([]byte("AUTOMATION_TAG:9teZH8JJs7dnmINc7PNf1HO3OFi.t7P6DquPRKmyfWoSySE9HtpPIF3zROHFKV24"))
 }
 
 func main() {
