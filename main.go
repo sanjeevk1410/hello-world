e63576a0051da00c53680a2f936d9c36a2fe8394
skaneria <sanjeev.kaneria@oracle.com>
2018-04-19 13:51:06 +0000

    CROW mini automation change

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	  w.Write([]byte("Hello WORLD!!\n"))
-       w.Write([]byte("AUTOMATION_TAG:J8ww9vg4v11CxINEZiseuG2OUrZsT.Qx2UclJtP0cSuxJDRoNR1nqWN.SiS7Nsid"))
+       w.Write([]byte("AUTOMATION_TAG:BW5L2YstTstwk4qaAbcuwPGsff10y5cTz611Nel4YK4ZsHnlDcc26cb9czYHCfdt"))
 }
 
 func main() {
